8ea8978ede3618e29be5d59cdfcba2410eb47c1b
Alessandro Tundo <alessandrotundo94@gmail.com>
2017-06-12 12:40:39 +0200

    Completed adls data discretization. Fixed non-unique sensors locations.
    Other minor fixes to discretize sensors data
    
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/discretize_adls_data.py
+++ src/discretize_adls_data.py
@@ -18,16 +18,16 @@ def next_timeslice(timeslice, secs):
 # Get all tables
 tables = cursor.execute('SELECT name FROM sqlite_master WHERE type="table" ORDER BY name').fetchall()
 
-# Get all sensors table
-sensors_tables = list()
+# Get all ADLs tables
+adls_tables = list()
 for table in tables:
-    if 'Sensors' in table[0]:
-        sensors_tables.append(table[0])
+    if 'ADLs' in table[0]:
+        adls_tables.append(table[0])
         # Drop old tables in order to avoid issues on script re-execution
-        old_sensor_table = table[0] + '_Observation_Vectors'
-        cursor.execute('DROP TABLE IF EXISTS ' + old_sensor_table)
+        old_adls_table = table[0] + '_Activity_States'
+        cursor.execute('DROP TABLE IF EXISTS ' + old_adls_table)
 
-for table in sensors_tables:
+for table in adls_tables:
     # Prepare queries to get absolute_start_time and absolute_end_time
     query_first = 'SELECT start_time FROM ' + table + ' ORDER BY start_time ASC LIMIT 1'
     query_last = 'SELECT end_time FROM ' + table + ' ORDER BY start_time DESC LIMIT 1'
@@ -38,29 +38,23 @@ for table in sensors_tables:
     last_row = cursor.execute(query_last)
     absolute_end_time = datetime.strptime(last_row.fetchone()[0], '%Y-%m-%d %H:%M:%S')
 
-    # Get available sensors
-    rows = cursor.execute('SELECT DISTINCT location FROM ' + table).fetchall()
-    available_sensors = ' INTEGER DEFAULT 0, '.join(str(row[0]) for row in rows)
+    # Create activity states table
+    cursor.execute('CREATE TABLE ' + table + '_Activity_States (timestamp TEXT, activity TEXT)')
 
-    # Create observation vectors table
-    cursor.execute('CREATE TABLE ' + table + '_Observation_Vectors (timestamp TEXT,' + available_sensors + ' INTEGER DEFAULT 0)')
-
-    # Set the timeslice and get active sensors
+    # Set the timeslice and get activities
     timeslice = absolute_start_time
     while (timeslice < absolute_end_time):
-        query = 'SELECT location, place FROM ' + table + ' WHERE datetime(start_time) <= ? AND datetime(end_time) >= ?'
-        rows = cursor.execute(query, [timeslice, timeslice]).fetchall()
-        
-        # Insert observation vector
-        if len(rows) == 0:
-            insert_query = 'INSERT INTO ' + table + '_Observation_Vectors(timestamp) VALUES (?)'
-        else:
-            active_sensors_keys = ','.join(str(row[0]) for row in rows)
-            active_sensors_values = ','.join(str(1) for row in rows)
-            insert_query = 'INSERT INTO ' + table + '_Observation_Vectors(timestamp,' + active_sensors_keys + ') VALUES (?,' + active_sensors_values + ')'
-
+        query = 'SELECT activity FROM ' + table + ' WHERE datetime(start_time) <= ? AND datetime(end_time) >= ?'
+        row = cursor.execute(query, [timeslice, timeslice]).fetchone()
         string_ts = timeslice.strftime("%Y-%m-%d %H:%M:%S")
-        cursor.execute(insert_query, [string_ts])
+
+        # Insert activity
+        if row is None:
+            insert_query = 'INSERT INTO ' + table + '_Activity_States(timestamp) VALUES (?)'
+            cursor.execute(insert_query, [string_ts])
+        else:
+            insert_query = 'INSERT INTO ' + table + '_Activity_States(timestamp,activity) VALUES (?,?)'
+            cursor.execute(insert_query, [string_ts, row[0]])
 
         timeslice = next_timeslice(timeslice, 60)
 conn.close()
