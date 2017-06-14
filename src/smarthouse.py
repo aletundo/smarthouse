95348980c9a7d037a6ec298d725f8d3d4748a634
Alessandro Tundo <alessandrotundo94@gmail.com>
2017-06-14 15:30:17 +0200

    Fixed utils/db execution path. Added missing init to hmm module.
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/smarthouse.py
+++ src/smarthouse.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
+import os
 from utils import txt_to_csv
 from preprocess import load_dataset, discretize_data
+from hmm import hmm_init
+from datetime import datetime
+
+# Change directory to the script directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 txt_to_csv.convert()
 load_dataset.load()
--- src/utils/db.py
+++ src/utils/db.py
@@ -1,11 +1,13 @@
 import sqlite3, os
 
 def get_conn():
+    # Change directory to the script directory
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
     db_dir = '../../db/'
     # Make db sub directory if not exists
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
 
     conn = sqlite3.connect(db_dir + 'smarthouse.db')
-    conn.row_factory = sqlite3.Row
     return conn
