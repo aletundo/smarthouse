Fixed matrices probabilities calculation
Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

src/hmm/hmm_init.py
@@ -14,13 +14,18 @@ def create_start_matrix(n_states = None, dist = []):
 
     print("\nStart matrix created! :)\n\n%s" % start_matrix)
     return np.asmatrix(start_matrix)
+def calc_probabilities(row):
+    sum = np.sum(row)
+    print np.sum(row/sum)
+    return row / sum
 
 def create_trans_matrix(states_seq = [], n_states = None):
     trans_matrix = np.zeros((n_states, n_states))
 
     for s in range(len(states_seq)-1):
-        trans_matrix[states_seq[s]-1, states_seq[s+1]-1] +=1
-    trans_matrix = trans_matrix / len(states_seq)
+        trans_matrix[states_seq[s]-1, states_seq[s+1]-1] += 1
+    trans_matrix = np.apply_along_axis( calc_probabilities, axis=1, arr=trans_matrix )
+    #trans_matrix = trans_matrix / len(states_seq)
 
     print("\nTransition matrix created! :)\n\n%s" % trans_matrix)
     return np.asmatrix(trans_matrix)
@@ -29,8 +34,9 @@ def create_em_matrix(states_seq = [], obs_seq = [], n_states = None, n_obs = Non
     em_matrix = np.zeros((n_states, n_obs))
 
     for s in range(len(states_seq)-1):
-        em_matrix[states_seq[s]-1, obs_seq[s]-1] +=1
-    em_matrix = em_matrix / len(obs_seq)
+        em_matrix[states_seq[s]-1, obs_seq[s]-1] += 1
+    #em_matrix = em_matrix / len(obs_seq)
+    em_matrix = np.apply_along_axis( calc_probabilities, axis=1, arr=em_matrix )
 
     print("\nEmission matrix created! :)\n\n%s" % em_matrix)
     return np.asmatrix(em_matrix)
@@ -96,6 +102,7 @@ def get_possible_obs(dataset):
 
 def build_obs_sequence(observation_rows, possible_obs):
     obs_list = []
+    obs_vectors = []
     for row in observation_rows:
         ob_vector = ''
         for col in range(len(row.keys())):
@@ -105,10 +112,12 @@ def build_obs_sequence(observation_rows, possible_obs):
             else:
                 ob_vector = ob_vector + str(row[col])
         obs_list.append(possible_obs[ob_vector])
+        obs_vectors.append(ob_vector)
 
     obs_seq = np.array(obs_list)
+    obs_vectors = np.array(obs_vectors)
     print("\nObservations sequence:\n%s\n" % obs_seq)
-    return obs_seq
+    return obs_seq, obs_vectors
 
 def build_states_sequence(state_rows, possible_states):
     states_list = []