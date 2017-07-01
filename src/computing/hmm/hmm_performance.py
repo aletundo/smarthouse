155ae3c83ff7023def5533f4a8ba11b89f0ffe14
Alessandro Tundo <alessandrotundo94@gmail.com>
2017-07-01 18:46:24 +0200

    Added observations sequences prob test with forward alg
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/computing/hmm/hmm_performance.py
+++ src/computing/hmm/hmm_performance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import numpy as np
-import hmm_init
+from random import randint
+import hmm_init, sqlite3
 from sklearn import metrics
 from hidden_markov import hmm
 from datetime import datetime, timedelta
@@ -62,6 +63,105 @@ def learning_curve(dataset, start_day, end_day):
         end_day_train = end_day_train + timedelta(days=1)
 
     return f_measure_list
+
+def get_obs_probability(dataset, observations, start_day, end_day):
+    possible_states, possible_states_array, possible_obs, possible_obs_array = hmm_init.build_possible_structures(dataset)
+
+    train_states_value_seq, _, train_obs_seq, _, _, _, _, _ = hmm_init.build_sets('demo', dataset, possible_states, possible_obs, start_day, end_day)
+
+    model = hmm_init.init_model(possible_states, possible_obs, possible_states_array, possible_obs_array, train_states_value_seq, train_obs_seq)
+
+    return model.forward_algo(observations)
+
+def build_random_obs(dataset):
+    random_obs = []
+    for i in range(5):
+        random_samples = []
+        for j in range(10):
+            possible_obs = hmm_init.get_possible_obs(dataset + '_Sensors_Observation_Vectors')
+            r = randint(1, len(possible_obs))
+
+            sample = ''
+            for k, v in possible_obs.iteritems():
+                if r == v:
+                    random_samples.append(k)
+        random_obs.append(random_samples)
+    return random_obs
+
+def build_datasetA_obs():
+    dataset_obs = []
+
+    conn = db.get_conn()
+    conn.row_factory = sqlite3.Row
+    conn.text_factory = str
+    cursor = conn.cursor()
+
+    rows = cursor.execute('SELECT * FROM OrdonezA_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2011, 11, 29, 0, 0, 59), datetime(2011, 11, 29, 0, 10, 59)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezA_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezA_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2011, 11, 29, 12, 17, 59), datetime(2011, 11, 29, 12, 27, 59)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezA_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezA_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2011, 12, 01, 11, 16, 59), datetime(2011, 12, 01, 11, 26, 59)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezA_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezA_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2011, 12, 03, 9, 0, 59), datetime(2011, 12, 03, 9, 10, 59)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezA_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezA_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2011, 12, 05, 20, 10, 59), datetime(2011, 12, 05, 20, 20, 59)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezA_Sensors_Observation_Vectors'))[1])
+
+    return dataset_obs
+
+def build_datasetB_obs():
+    dataset_obs = []
+
+    conn = db.get_conn()
+    conn.row_factory = sqlite3.Row
+    conn.text_factory = str
+    cursor = conn.cursor()
+
+    rows = cursor.execute('SELECT * FROM OrdonezB_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2012, 11, 12, 8, 0, 0), datetime(2012, 11, 12, 8, 10, 0)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezB_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezB_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2012, 11, 13, 16, 7, 0), datetime(2012, 11, 13, 16, 17, 0)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezB_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezB_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2012, 11, 20, 15, 14, 0), datetime(2012, 11, 20, 15, 24, 0)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezB_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezB_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2012, 11, 13, 23, 17, 0), datetime(2012, 11, 23, 23, 27, 0)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezB_Sensors_Observation_Vectors'))[1])
+    rows = cursor.execute('SELECT * FROM OrdonezB_Sensors_Observation_Vectors WHERE timestamp BETWEEN ? AND ? LIMIT 10', [datetime(2012, 12, 01, 23, 0, 0), datetime(2012, 12, 01, 23, 10, 0)]).fetchall()
+    dataset_obs.append(hmm_init.build_obs_sequence(rows, hmm_init.get_possible_obs('OrdonezB_Sensors_Observation_Vectors'))[1])
+
+    return dataset_obs
+
+def build_manualA_obs():
+    manual_obs = [
+    ['000000000010', '000100000000', '000000000010', '000100000000', '000000000010', '000100000000', '000000000010', '000100000000', '000000000010', '000100000000'],
+    ['000000000001', '000000000001', '000000000001', '000000000001', '000000000001', '000000000001', '000000000001', '000000000001', '000000000001', '000000000001'],
+    ['000001000000', '100000000000', '000001000000', '100000000000', '000001000000', '100000000000', '000001000000', '100000000000', '000001000000', '100000000000'],
+    ['000000000000', '000001000000', '000000000000', '000001000000', '000000000000', '000001000000', '000000000000', '000001000000', '000000000000', '000001000000'],
+    ['000010000000', '000000000100', '000000000001', '100000000000', '000010000000', '000000000100', '000000000001', '100000000000', '000010000000', '000000000100'],
+    ]
+    return manual_obs
+
+def build_manualB_obs():
+    manual_obs = [
+    ['000000000000', '000000000001', '000000000010', '000000000000', '000000000001', '000000000010', '000000000000', '000000000001', '000000000010', '000000000000'],
+    ['100000000000', '100000000000', '000000100000', '000000100000', '100000000000', '100000000000', '000000100000', '000000100000', '100000000000', '100000000000'],
+    ['000000100000', '000000010000', '000000100000', '000000010000', '000000100000', '000000010000', '000000100000', '000000010000', '000000100000', '000000010000'],
+    ['110000000000', '000010000000' ,'110000000000', '110000000000', '000010000000' ,'110000000000', '110000000000', '000010000000' ,'110000000000', '110000000000'],
+    ['010000000000', '010000000000', '010000000000', '010000000000', '010000000000', '010000000000', '010000000000', '010000000000', '010000000000', '010000000000']
+    ]
+    return manual_obs
+
+def obs_probability_test(dataset, start_day, end_day):
+    random_obs = build_random_obs(dataset)
+    dataset_obs = build_datasetA_obs() if dataset == 'OrdonezA' else build_datasetB_obs()
+    manual_obs = build_manualA_obs() if dataset == 'OrdonezA' else build_manualB_obs()
+    random_results = []
+    dataset_results = []
+    manual_results = []
+    for i in range(5):
+        random_results.append(get_obs_probability(dataset, random_obs[i], start_day, end_day))
+        dataset_results.append(get_obs_probability(dataset, dataset_obs[i], start_day, end_day))
+        manual_results.append(get_obs_probability(dataset, manual_obs[i], start_day, end_day))
+    return random_results, dataset_results, manual_results
 
 #Calculate performance for each dataset and return average fm measure and labels accuracy
 def final_test():
